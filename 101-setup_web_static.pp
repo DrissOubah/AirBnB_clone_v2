0cb409d8f280017b2e99a6d10f59da4c429947d3
DrissOubah <drissoubahmane@gmail.com>
2024-07-14 18:48:52 -0700

    101

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -3,25 +3,32 @@
 # Configures a web server for deployment of web_static.
 
 # Nginx configuration file
-$nginx_conf = "server {
+$nginx_conf = @("EOF"
+server {
     listen 80 default_server;
     listen [::]:80 default_server;
-    add_header X-Served-By \$hostname;
+    add_header X-Served-By $hostname;
     root   /var/www/html;
     index  index.html index.htm;
+
     location /hbnb_static {
         alias /data/web_static/current;
         index index.html index.htm;
     }
+
     location /redirect_me {
         return 301 https://www.google.com;
     }
+
     error_page 404 /404.html;
+
     location /404 {
-      root /var/www/html;
-      internal;
+        root /var/www/html;
+        internal;
     }
-}"
+}
+"EOF
+)
 
 package { 'nginx':
   ensure   => 'present',
@@ -113,4 +120,3 @@ service { 'nginx':
   enable    => true,
   subscribe => File['/etc/nginx/sites-available/default'],
 }
-
